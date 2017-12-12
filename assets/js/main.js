7e21f518ae083d9623815a4e5d3e58a78f98b504
lpalominosf <lorena.palominos@gmail.com>
2017-12-12 09:22:50 -0300

    correccion de comentarios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,69 @@
 $(document).ready(function(){
-		//Cuenta diapositivas, mostrando la cantidad de las imágenes
+		/*
+		Cuenta diapositivas, mostrando la cantidad de las imágenes
+		*/
 		var imgItems = $('.slider li').length;
 		var imgPosition = 1;
-		//Este for agrega circulos dependiendo de la cantidad de imagenes
-		//Que se encuentren en las dispositivas
+		/*
+		*Este for agrega circulos dependiendo de la cantidad de imagenes
+		*Que se encuentren en las dispositivas
+		*/
 		for(i=1; i <= imgItems; i++){
 			$('.pagination').append('<li><span class="fa fa-circle"></span></li>');
 		}
 
-		
-		//Ocultar slides
+		/*
+		Ocultar slides
+		*/
 		$('.slider li').hide();
-		//Mostrar primer slide
+		/*
+		Mostrar primer slide
+		*/
 		$('.slider li:first').show();
-		//Asignar un color a los circulos
+		/*
+		Asignar un color a los circulos
+		*/
 		$('.pagination li:first').css({'color': '#cd6e2e'});
-
-		//Al dar click se muestra el slide correspondiente
+		/*
+		Al dar click se muestra el slide correspondiente
+		*/
 		$('.pagination li').click(pagination);
-		//Siguiente imagen
+		/*
+		Siguiente imagen
+		*/
 		$('.right span').click(nextSlider);
-		//Imagen anterior
+		/*
+		Imagen anterior
+		*/
 		$('.left span').click(prevSlider);
 
 		setInterval(function(){
 			nextSlider();
 		}, 4000);
 
-		//funciones
+		/*
+		Funciones
+		*/
 		function pagination(){
-			//Llama a la posicion de la paginacion a la que se le da click
-			//+1 muestra la posición exacta, sin el 0
+			/*
+			*Llama a la posicion de la paginacion a la que se le da click
+			*+1 muestra la posición exacta, sin el 0
+			*/
 			var paginationPos = $(this).index() +1;
-
-			//Esconde las imagenes
+			/*
+			Esconde las imagenes
+			*/
 			$('.slider li').hide();
-			// fadeIn=Efecto / paginationPos=numero de la posicion al que 
-			// le hemos dado click
+			/*
+			*fadeIn=Efecto / paginationPos=numero de la posicion al que 
+			*le hemos dado click
+			*/
 			$('.slider li:nth-child('+ paginationPos +')').fadeIn();
 
 			$('.pagination li').css({'color': '#858585;'});
-			//Al hacer click queda como seleccionado del color dado
+			/*
+			Al hacer click queda como seleccionado del color dado
+			*/
 			$(this).css({'color': '#cd6e2e'});
 
 			imgPosition = paginationPos;
@@ -48,7 +71,9 @@ $(document).ready(function(){
 
 		function nextSlider(){
 
-			//Vuelve a número 1 la imagen seleccionada con la flecha derecha
+			/*
+			Vuelve a número 1 la imagen seleccionada con la flecha derecha
+			*/
 			if(imgPosition >= imgItems){
 				imgPosition = 1
 			} else {
@@ -57,18 +82,26 @@ $(document).ready(function(){
 
 			$('.pagination li').css({'color': '#858585;'});
 			$('.pagination li:nth-child('+ imgPosition +')').css({'color':'#cd6e2e'});
-			
-			//Esconde las imagenes
+			/*
+			Esconde las imagenes
+			*/
 			$('.slider li').hide();
-			// fadeIn=Efecto / paginationPos=numero de la posicion al que 
-			// le hemos dado click
+			/*
+			*fadeIn=Efecto / paginationPos=numero de la posicion al que 
+			*le hemos dado click
+			*/
 			$('.slider li:nth-child('+ imgPosition +')').fadeIn();
 
 		}
-		//Funcion que muestra la imagen previa
+		/*
+		Funcion que muestra la imagen previa
+		*/
 		function prevSlider(){
 
-			//Vuelve a número 1 la imagen seleccionada con la flecha izquierda
+
+			/*
+			Vuelve a número 1 la imagen seleccionada con la flecha izquierda
+			*/
 			if(imgPosition <= 1){
 				imgPosition = imgItems;
 			} else {
@@ -78,13 +111,21 @@ $(document).ready(function(){
 			$('.pagination li').css({'color': '#858585;'});
 			$('.pagination li:nth-child('+ imgPosition +')').css({'color':'#cd6e2e'});
 			
-			//Esconde las imagenes
+			/*
+			Esconde las imagenes
+			*/
 			$('.slider li').hide();
-			// fadeIn=Efecto / paginationPos=numero de la posicion al que 
-			// le hemos dado click
+			/*
+			*fadeIn=Efecto / paginationPos=numero de la posicion al que
+			*le hemos dado click
+			*/
 			$('.slider li:nth-child('+ imgPosition +')').fadeIn();
 
 		}
 
 
 });
+
+/*
+*Error en los colores
+*/
